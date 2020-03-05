644bf470adc460139641cfee41569006f5a23dae
Mathieu Robichaud <mathieu@standish.ca>
2020-03-05 16:16:42 -0500

    Cleanup

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Pokemon, Generation, Region, UserPokemon
 
+
 admin.site.register(Pokemon)
 admin.site.register(Generation)
 admin.site.register(Region)
--- pokemon/translation.py
+++ pokemon/translation.py
@@ -2,7 +2,9 @@ from modeltranslation.translator import translator, TranslationOptions
 
 from .models import Pokemon
 
+
 class PokemonTranslationOptions(TranslationOptions):
 	fields = ['name']
+
 
 translator.register(Pokemon, PokemonTranslationOptions)
