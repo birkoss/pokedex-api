687b5ae7634fb3a4ce14022d8d8d884d3eaad1f4
Mathieu Robichaud <mathieu@standish.ca>
2020-03-07 08:44:08 -0500

    UI Tweak for the options modal

--- base/static/base/js/app.js
+++ base/static/base/js/app.js
@@ -1,5 +1,60 @@
-jQuery(document).ready(function() {
+/* Use to keep the existing Modal Options when it's first open */
+var original_modal_options = {};
 
+
+/* Load the options for this pokemon and show the modal */
+function pokemon_show_modal(pokemon_number) {
+	jQuery('#pokemon-modal .modal-body').load(ajax_single_option.replace("0000", pokemon_number), function() {
+		jQuery('#pokemon-modal').modal({show:true});
+		jQuery('[data-toggle="tooltip"]').tooltip();
+		jQuery("#pokemon-modal .btn-save").prop("disabled", true);
+
+		original_modal_options = JSON.stringify(get_modal_options());
+
+		/* Activate/Disable the SAVE button when options are changed */
+		jQuery("#pokemon-modal input[type='checkbox']").change(function() {
+			var current_modal_options = JSON.stringify(get_modal_options());
+
+			jQuery("#pokemon-modal .btn-save").prop("disabled", (original_modal_options == current_modal_options));
+		});
+	});
+
+	return false;
+}
+
+
+/* Save the options for this Pokemon */
+function pokemon_save_options(pokemon_number, options, callback) {
+	jQuery.ajax({
+		type: "POST",
+		url: ajax_single_option.replace("0000", pokemon_number),
+		data: {
+			"options": JSON.stringify(options),
+			"csrfmiddlewaretoken": ajax_csrf_token
+		},
+		success: function(ret) {
+			if (callback != undefined) {
+				callback(ret);
+			}
+		}
+	});
+}
+
+
+/* Get all the options (name and value) from the modal */
+function get_modal_options() {
+	modal_options = {};
+
+	jQuery("#pokemon-modal input[type='checkbox']").each(function() {
+		modal_options[ jQuery(this).attr("id") ] = jQuery(this).prop("checked");
+	});
+
+	return modal_options;
+}
+
+
+jQuery(document).ready(function() {
+	/* Bind the SAVE button in the modal */
 	jQuery("#pokemon-modal .btn-save").click(function() {
 		/* Do NOT allow saving while it's already in process */
 		if (jQuery("#pokemon-modal .btn-save").prop("disabled")) {
@@ -8,15 +63,12 @@ jQuery(document).ready(function() {
 		jQuery("#pokemon-modal .btn-save").prop("disabled", true).html("Saving...");
 
 		/* Fetch all options from the modal */
-		var options = {};
-		jQuery("#pokemon-modal input[type='checkbox']").each(function() {
-			options[ jQuery(this).attr("id") ] = jQuery(this).prop("checked");
-		});
+		var options = get_modal_options();
 
 		var pokemon_number = jQuery("#pokemon-modal input[name='pokemon_number']").val();
 
 		/* Update all options for this pokemon */
-		pokemon_toggle_options(pokemon_number, options, function(ret) {
+		pokemon_save_options(pokemon_number, options, function(ret) {
 			jQuery("#pokemon-modal .btn-save").prop("disabled", false).html("Save");
 			jQuery('#pokemon-modal').modal('hide');
 
@@ -30,6 +82,7 @@ jQuery(document).ready(function() {
 		});
 	});
 
+	/* Load the first Pokemon page */
 	if (jQuery(".main-pokemons-list").length) {
 		jQuery.ajax({
 			'type': "GET",
@@ -54,61 +107,3 @@ jQuery(document).ready(function() {
 		});
 	}
 });
-
-function pokemon_change_option(pokemon_number, option_name) {
-	jQuery(".container-pokemon-" + pokemon_number + " td.actions > .btn-mark-owned").hide();
-	jQuery(".container-pokemon-" + pokemon_number + " td.actions").prepend('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="display: block; shape-rendering: auto;" width="60px" height="60px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid"><circle cx="59.6265" cy="50" fill="#e83a38" r="20"><animate attributeName="cx" repeatCount="indefinite" dur="1s" keyTimes="0;0.5;1" values="30;70;30" begin="-0.5s"></animate></circle><circle cx="40.3735" cy="50" fill="#4c4b49" r="20"><animate attributeName="cx" repeatCount="indefinite" dur="1s" keyTimes="0;0.5;1" values="30;70;30" begin="0s"></animate></circle><circle cx="59.6265" cy="50" fill="#e83a38" r="20"><animate attributeName="cx" repeatCount="indefinite" dur="1s" keyTimes="0;0.5;1" values="30;70;30" begin="-0.5s"></animate><animate attributeName="fill-opacity" values="0;0;1;1" calcMode="discrete" keyTimes="0;0.499;0.5;1" dur="1s" repeatCount="indefinite"></animate></circle></svg>');
-
-	var options = {};
-	options[option_name] = -1;
-
-	pokemon_toggle_options(pokemon_number, options, function(ret) {
-		if (ret['status'] == "ok") {
-			jQuery(".container-pokemon-" + pokemon_number + " td.actions svg").remove();
-			jQuery(".container-pokemon-" + pokemon_number + " td.actions > .btn-options").show();
-		}
-	});
-
-	return false;
-}
-
-function pokemon_show_modal(pokemon_number) {
-	jQuery('#pokemon-modal .modal-body').load(ajax_single_option.replace("0000", pokemon_number), function() {
-		jQuery('#pokemon-modal').modal({show:true});
-		jQuery('[data-toggle="tooltip"]').tooltip();
-	});
-
-	return false;
-}
-
-function pokemon_toggle_options(pokemon_number, options, callback) {
-	jQuery.ajax({
-		type: "POST",
-		url: ajax_single_option.replace("0000", pokemon_number),
-		data: {
-			"options": JSON.stringify(options),
-			"csrfmiddlewaretoken": ajax_csrf_token
-		},
-		success: function(ret) {
-			if (callback != undefined) {
-				callback(ret);
-			}
-		}
-	});
-}
-
-function set_options(type, value) {
-	jQuery.ajax({
-		type: "POST",
-		url: ajax_single_option.replace("0000", pokemon_number),
-		data: {
-			"options": JSON.stringify(options),
-			"csrfmiddlewaretoken": ajax_csrf_token
-		},
-		success: function(ret) {
-			if (callback != undefined) {
-				callback(ret);
-			}
-		}
-	});
-}
