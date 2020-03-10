a5ba656244d5cb21ca3ec8615fa2b22ff65d2081
Mathieu Robichaud <mathieu@standish.ca>
2020-03-10 19:50:54 -0400

    CSS tweaks

--- base/static/base/js/app.js
+++ base/static/base/js/app.js
@@ -260,9 +260,9 @@ function status_update_pokedex_stats(pokedex_stats) {
 	} else {
 		/* Logged user without HIDE filter */
 		if (pokedex_stats['hide'] == 0) {
-			content = "Remaining: " + (pokedex_stats['total']-pokedex_stats['count_is_owned']) + " / " + pokedex_stats['total'] + " Pokemon(s)";
+			content = "<span class='prefix'>Remaining: </span>" + (pokedex_stats['total']-pokedex_stats['count_is_owned']) + " / " + pokedex_stats['total'] + "<span class='suffix'> Pokemon(s)</span>";
 		} else {
-			content = "Remaining: " + pokedex_stats['current'] + " / " + pokedex_stats['total'] + " Pokemon(s)";
+			content = "<span class='prefix'>Remaining: </span>" + pokedex_stats['current'] + " / " + pokedex_stats['total'] + "<span class='suffix'> Pokemon(s)</span>";
 		}
 	}
 
